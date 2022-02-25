992912583c849320f757478fa9ee3ed840d9d598
gutgab <gutgab98@gmail.com>
2022-02-24 22:42:34 -0400

    add display function

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let readIcon = document.createElement("img");
+let favIcon = document.createElement("img");
+let deleteIcon = document.createElement("img");
+let myLibrary = [];
+let bookBtns = document.createElement("div");
+let grid = document.querySelector(".grid");
+
 function Book(title,author,pages,read){
     this.title = title;
     this.author = author;
@@ -9,3 +16,31 @@ function Book(title,author,pages,read){
     }
 }
 
+function addBook(title,author,pages,read){
+    let obj = new Book(title,author,pages,read);
+    myLibrary.push(obj);
+}
+
+function displayBooks(){
+    let fragment = new DocumentFragment();
+    myLibrary.forEach(book => {
+        let div = document.createElement("div");
+        let par = document.createElement("p");
+        div.classList.add("book");
+        par.innerText = `${book.title} by ${book.author},${book.pages} pages,${book.read == true ? "read":"not read yet"}`;
+        div.appendChild(par);
+        div.appendChild(bookBtns);
+        grid.appendChild(div);
+    }
+    );
+}
+
+readIcon.setAttribute("src","./assets/read-on.png");
+readIcon.setAttribute("alt","read");
+favIcon.setAttribute("src","/assets/star-plus-outline.png");
+favIcon.setAttribute("alt","fav");
+deleteIcon.setAttribute("src","/assets/delete.png");
+deleteIcon.setAttribute("alt","delte");
+bookBtns.appendChild(readIcon);
+bookBtns.appendChild(favIcon);
+bookBtns.appendChild(deleteIcon);
